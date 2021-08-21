2260ead7cf9b70e26820146f9ea10635d5677447
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-08-21 14:43:43 +0530

    updated

--- sys/bin/alphabat.asm
+++ sys/bin/alphabat.asm
@@ -1,8 +1,16 @@
-mov ah, 0x0e
-mov al, 65
-int 0x80
+int 0x10
 
 loop:
-    inc al, 1
-    int 0x80
-    jmp loop
+        inc al
+        cmp al, 'Z' + 1
+        je exit
+        int 0x10
+        jmp loop
+
+exit:
+    jmp $
+
+; BIOS
+jmp $
+times 510-($-$$) db 0
+db 0x55, 0xaa
