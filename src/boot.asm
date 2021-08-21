72e8c066aa566a2400ac1cb4279fad56c5c4811c
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-08-22 03:18:13 +0530

    src updated

--- src/boot.asm
+++ src/boot.asm
@@ -1,14 +1,16 @@
-[org 0x7c00]
+clearscreen:
+    mov ax, 0x07
+    mov bh, 0x07
+    mov cx, 0x00
+    mov dx, 0x184f
+    int 0x10
 
-;mov ax, 0x07
-;mov bh, 0x07
-;mov cx, 0x00
-;mov dx, 0x184f
-;int 0x10
+bosintro:
+    [org 0x7c00]
+    mov ah, 0x0e
+    mov al, [stringlabal]
+    int 0x10
 
-mov ah, 0x0e
-mov al, [stringlabal]
-mov bh, 0x10
 jmp $
 
 
@@ -17,6 +19,6 @@ stringlabal:
 
 ; Boot Loader 512 bytes-long
 ; ** Added boot signature bits 0x55 0xaa
-jmp $
+
 times 510-($-$$) db 0
 db 0x55, 0xaa
