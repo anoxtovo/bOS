0309093d8338a2e87f4540f42db44c257545a7ac
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-08-22 03:32:56 +0530

    src updated

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,4 @@
+; clear screen
 clearscreen:
     mov ax, 0x07
     mov bh, 0x07
@@ -5,17 +6,24 @@ clearscreen:
     mov dx, 0x184f
     int 0x10
 
-bosintro:
-    [org 0x7c00]
-    mov ah, 0x0e
-    mov al, [stringlabal]
-    int 0x10
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, stringlabal
 
+bosintro:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp bosintro
+
+end:
+    
 jmp $
 
-
 stringlabal:
-    db "Basic Operating System - bOS [ v0.1 ]", 0
+    db "Basic Operating System - bOS [v0.1]", 0
 
 ; Boot Loader 512 bytes-long
 ; ** Added boot signature bits 0x55 0xaa
