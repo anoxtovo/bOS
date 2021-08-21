1281241d946bb2d58eb1f7d4d336cc80c447d5ed
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-08-21 19:14:48 +0530

    file tree updated

--- src/boot.asm
+++ src/boot.asm
@@ -5,26 +5,6 @@ mov ah, 0x0e
 mov al, 'H'
 int 0x10
 
-; Print E
-mov ah, 0x0e
-mov al, 'E'
-int 0x10
-
-; Print L
-mov ah, 0x0e
-mov al, 'L'
-int 0x10
-
-; Print L
-mov ah, 0x0e
-mov al, 'L'
-int 0x10
-
-; Print O
-mov ah, 0x0e
-mov al, 'O'
-int 0x10
-
 ; BIOS
 jmp $
 
