824084c679fd98baf774936a3f4457e58d26961b
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-08-22 02:58:42 +0530

    src updated

--- src/boot.asm
+++ src/boot.asm
@@ -1,12 +1,18 @@
-; Printing 'Hello' after the booting
+; clear screen
+clearscreen:
+    mov ax, 0x07
+    mov bh, 0x07
+    mov cx, 0x00
+    mov dx, 0x184f
+    int 0x10
 
-; Print H
-mov ah, 0x0e
-mov al, 'H'
-int 0x10
 
-; BIOS
+;mov ah, 0x0e
+;mov al, "b"
+;int 0x10
+
+; Boot Loader 512 bytes-long
+; ** Added boot signature bits 0x55 0xaa
 jmp $
-
 times 510-($-$$) db 0
 db 0x55, 0xaa
