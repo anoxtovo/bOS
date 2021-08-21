f0b67675e4142be4e80d75cd433ed192c41b940c
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-08-21 14:50:45 +0530

    updated

--- sys/bin/alphabat.asm
+++ sys/bin/alphabat.asm
@@ -1,16 +1,22 @@
+;   author      : Thumula Basura Suraweera
+;   version     : v.1
+;   codename    : Basic Operating System (bOS)
+
+; BIOS intruption
 int 0x10
 
 loop:
-        inc al
-        cmp al, 'Z' + 1
-        je exit
+        inc al              ; increment for al value
+        cmp al, 'Z' + 1     ; compare if al == Z + 1
+        je exit             ; if condition true exit
         int 0x10
-        jmp loop
+        jmp loop            ; jump to the loop
 
 exit:
     jmp $
 
-; BIOS
+; BIOS sector with 510 null bytes
+; set last byte value to 0x55, 0xaa
 jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
